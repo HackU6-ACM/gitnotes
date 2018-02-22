93dbc7a3dba2780bdb4a7added1f4d78176c6d11
Timothy Baker <tbake0155@gmail.com>
2018-02-22 11:38:30 -0500

    add test .attributes file

--- popup.js
+++ popup.js
@@ -4,3 +4,12 @@
 var currentURL = window.location.href;
 
 document.body.innerHTML = currentURL;
+
+var attributes = currentURL + "/.attributes"
+
+var client = new XMLHttpRequest();
+client.open('GET', attributes);
+client.onreadystatechange = function() {
+    alert(client.responseText);
+}
+client.send();
