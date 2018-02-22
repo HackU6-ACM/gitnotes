f3e1118ebf2c314895b4f258626d2ecece801339
Timothy Baker <tbake0155@gmail.com>
2018-02-22 12:23:45 -0500

    js

--- popup.js
+++ popup.js
@@ -1,15 +1,14 @@
 
-
-
 var currentURL = window.location.href;
+var rawURL = currentURL.replace("/https://github.com/", "https://raw.githubusercontent.com/");
+var attributes = rawURL + "/.attributes"
 
-document.body.innerHTML = currentURL;
-
-var attributes = currentURL + "/.attributes"
+document.body.innerHTML = attributes;
 
 var client = new XMLHttpRequest();
 client.open('GET', attributes);
 client.onreadystatechange = function() {
-    alert(client.responseText);
+   var response =client.responseText;
+   document.body.innerHTML = response;
 }
 client.send();
