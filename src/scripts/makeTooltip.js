ef31707dcd09fe12ef040d31ff6fe8d60d475286
Patrick T Cox <pxcox003@odu.edu>
2018-02-22 18:07:05 -0500

    Added third party tooltip
    Renames tooltip.js to makeTooltip.js

---
+++ src/scripts/makeTooltip.js
@@ -0,0 +1,36 @@
+String.prototype.replaceAll = function(searchStr, replaceStr) {
+  var str = this;
+
+  // No match exists in string
+  if ( str.indexOf(searchStr) === -1 ) {
+    return str;
+  }
+
+  // Replace and remove the first match
+  return ( str.replace(searchStr, replaceStr)).replaceAll(searchStr, replaceStr);
+}
+
+function makeTooltip(attributeFileURL, input, thisFileName) {
+  // Load JSON file into dictionary
+  $.getJSON(attributeFileURL, function(data) {
+    var master = {};
+    $.each( data, function( mkey, mval ) {
+      var sub = {}
+      $.each( mkey, function( skey, sval ) {
+        sub[skey] = sval
+      });
+      master[mkey] = sub
+    });
+  });
+
+  dict = master[thisFileName]
+
+  // For each key replace instaces in input with tooltip containing value
+  for ( var key in dict ) {
+    var value = dict[key];
+
+    keyTooltip = "<button type=\"button\" class=\"btn btn-default btn-huge\" data-placement=\"top\" title=\"Tooltip on left\" onmouseenter=\"$(this).tooltip('show')\">tooltip</button>"
+  }
+
+  return input.replaceAll(key, keyTooltip)
+}
--- src/scripts/tooltip.js
+++ src/scripts/tooltip.js
@@ -1,36 +1,156 @@
-String.prototype.replaceAll = function(searchStr, replaceStr) {
-  var str = this;
+/**
+ * Tooltip.js
+ * A basic script that applies a mouseover tooltip functionality to all elements of a page that have a data-tooltip attribute
+ * Matthias Schuetz, http://matthiasschuetz.com
+ *
+ * Copyright (C) Matthias Schuetz
+ * Free to use under the MIT license
+ */
 
-  // No match exists in string
-  if ( str.indexOf(searchStr) === -1 ) {
-    return str;
-  }
+(function (root, factory) {
+	if (typeof define === "function" && define.amd) {
+		// AMD. Register as an anonymous module.
+		define(factory);
+	} else if (!root.tooltip) {
+		// Browser globals
+		root.tooltip = factory(root);
+	}
+}(this, function() {
+	var _options = {
+		tooltipId: "tooltip",
+		offsetDefault: 15
+	};
 
-  // Replace and remove the first match
-  return ( str.replace(searchStr, replaceStr)).replaceAll(searchStr, replaceStr);
-}
+	var _tooltips = [];
+	var _tooltipsTemp = null;
 
-function makeTooltip(attributeFileURL, input, thisFileName) {
-  // Load JSON file into dictionary
-  $.getJSON(attributeFileURL, function(data) {
-    var master = {};
-    $.each( data, function( mkey, mval ) {
-      var sub = {}
-      $.each( mkey, function( skey, sval ) {
-        sub[skey] = sval
-      });
-      master[mkey] = sub
-    });
-  });
+	function _bindTooltips(resetTooltips) {
+		if (resetTooltips) {
+			_tooltipsTemp = _tooltips.concat();
+			_tooltips = [];
+		}
 
-  dict = master[thisFileName]
+		Array.prototype.forEach.call(document.querySelectorAll("[data-tooltip]"), function(elm, idx) {
+			var tooltipText = elm.getAttribute("title").trim();
+			var options;
 
-  // For each key replace instaces in input with tooltip containing value
-  for ( var key in dict ) {
-    var value = dict[key];
+			if (resetTooltips && _tooltipsTemp.length && _tooltipsTemp[idx] && _tooltipsTemp[idx].text) {
+				if (tooltipText.length === 0) {
+					elm.setAttribute("title", _tooltipsTemp[idx].text);
+					tooltipText = _tooltipsTemp[idx].text;
+				}
 
-    keyTooltip = "<button type=\"button\" class=\"btn btn-default btn-huge\" data-placement=\"top\" title=\"Tooltip on left\" onmouseenter=\"$(this).tooltip('show')\">tooltip</button>"
-  }
+				elm.removeEventListener("mousemove", _onElementMouseMove);
+				elm.removeEventListener("mouseout", _onElementMouseOut);
+				elm.removeEventListener("mouseover", _onElementMouseOver);
+			}
 
-  return input.replaceAll(key, keyTooltip)
-}
+			if (tooltipText) {
+				elm.setAttribute("title", "");
+				elm.setAttribute("data-tooltip-id", idx);
+				options = _parseOptions(elm.getAttribute("data-tooltip"));
+				
+				_tooltips[idx] = {
+					text: tooltipText,
+					options: options
+				};
+
+				elm.addEventListener("mousemove", _onElementMouseMove);
+				elm.addEventListener("mouseout", _onElementMouseOut);
+				elm.addEventListener("mouseover", _onElementMouseOver);
+			}
+		});
+
+		if (resetTooltips) {
+			_tooltipsTemp = null;
+		}
+	}
+
+	function _createTooltip(text, tooltipId) {
+		var tooltipElm = document.createElement("div");
+		var tooltipText = document.createTextNode(text);
+		var options = tooltipId && _tooltips[tooltipId] && _tooltips[tooltipId].options;
+
+		if (options && options["class"]) {
+			tooltipElm.setAttribute("class", options["class"]);
+		}
+
+		tooltipElm.setAttribute("id", _options.tooltipId);
+		tooltipElm.appendChild(tooltipText);
+
+		document.querySelector("body").appendChild(tooltipElm);
+	}
+
+	function _getTooltipElm() {
+		return document.querySelector("#" + _options.tooltipId);
+	}
+
+	function _onElementMouseMove(evt) {
+		var tooltipId = this.getAttribute("data-tooltip-id");
+		var tooltipElm = _getTooltipElm();
+		var options = tooltipId && _tooltips[tooltipId] && _tooltips[tooltipId].options;
+		var offset = options && options.offset || _options.offsetDefault;
+		var scrollY = window.scrollY || window.pageYOffset;
+		var scrollX = window.scrollX || window.pageXOffset;
+		var tooltipTop = evt.pageY + offset;
+		var tooltipLeft = evt.pageX + offset;
+
+		if (tooltipElm) {
+			tooltipTop = (tooltipTop - scrollY + tooltipElm.offsetHeight + 20 >= window.innerHeight ? (tooltipTop - tooltipElm.offsetHeight - 20) : tooltipTop);
+			tooltipLeft = (tooltipLeft - scrollX + tooltipElm.offsetWidth + 20 >= window.innerWidth ? (tooltipLeft - tooltipElm.offsetWidth - 20) : tooltipLeft);
+
+			tooltipElm.style.top = tooltipTop + "px";
+			tooltipElm.style.left = tooltipLeft + "px";
+		}
+	}
+
+	function _onElementMouseOut(evt) {
+		var tooltipElm = _getTooltipElm();
+
+		if (tooltipElm) {
+			document.querySelector("body").removeChild(tooltipElm);
+		}
+	}
+
+	function _onElementMouseOver(evt) {
+		var tooltipId = this.getAttribute("data-tooltip-id");
+		var tooltipText = tooltipId && _tooltips[tooltipId] && _tooltips[tooltipId].text;
+
+		if (tooltipText) {
+			_createTooltip(tooltipText, tooltipId);	
+		}
+	}
+
+	function _parseOptions(options) {
+		var optionsObj;
+
+		if (options.length) {
+			try {
+				optionsObj = JSON.parse(options.replace(/'/ig, "\""));
+			} catch(err) {
+				console.log(err);
+			}
+		}
+
+		return optionsObj;
+	}
+
+	function _init() {
+		window.addEventListener("load", _bindTooltips);
+	}
+
+	_init();
+
+	return {
+		setOptions: function(options) {
+			for (var option in options) {
+				if (_options.hasOwnProperty(option)) {
+					_options[option] = options[option];
+				}
+			}
+		},
+		refresh: function() {
+			_bindTooltips(true);
+		}
+	};
+}));
