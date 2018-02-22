3192746c11e3b11f1cf8dbeafdb3cf49ea978e00
Timothy Baker <tbake0155@gmail.com>
2018-02-22 11:12:33 -0500

    javascript loads on github.com load

--- popup.js
+++ popup.js
@@ -1,25 +1,8 @@
 
 
-
-function getCurrentTabUrl(callback){
-
-    var queryInfo = {
-        active: true,
-        currentWindow: true
-    };
-
-    chrome.tabs.query(queryInfo, (tabs) => {
-        var tab = tabs[0];
-        var url = tab.url;
-        console.assert(typeof url == 'string', 'tab.url should be a string');
-        callback(url);
-    });
+function empty()
+{
+    document.body.innerHTML = "EMPTIED";
 }
 
-var script = ''
-
-getCurrentTabUrl((url)  => {
-    chrome.tabs.executeScript({
-        code: script
-    });
-});
+empty();
