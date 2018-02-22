6c13e96d207a2dad672ede5f404418b185471217
Timothy Baker <tbake0155@gmail.com>
2018-02-22 15:47:06 -0500

    changed attributes to gsv format

--- popup.js
+++ popup.js
@@ -1,17 +1,16 @@
 
 var currentURL = window.location.href;
 var rawURL = currentURL.replace("https://github.com/", "https://raw.githubusercontent.com/");
-var attributes = rawURL + "/.attributes";
+rawURL = rawURL.substring(0, rawURL.lastIndexOf('/'));
+var attributes = rawURL + "/master/.attributes";
 
 var client = new XMLHttpRequest();
 client.open('GET', attributes);
 client.onreadystatechange = function() {
    attributes = client.responseText;
-   //document.body.innerHTML = attributes;
+   document.body.innerHTML = attributes;
 }
 client.send();
-
-
 
 var doc = document.body.innerHTML;
 
@@ -23,12 +22,12 @@ var cut_ending = doc.indexOf(tbody_end);
 
 var body = doc.substring(cut_beginning, cut_ending);
 
-var mykey = dict[key];
-var re = new RegExp(dict[key], "g");
 
-var mod_body = body.replace(re, "<div class='highlighted' <a href='https://www.google.com'> test </a></div>");
+
+
+var mod_body = body.replace(/printer/g, "<div class='highlighted' <a href='https://www.google.com'> test </a></div>");
 
 doc = doc.replace(body, mod_body);
 
-document.body.innerHTML = doc;
+//document.body.innerHTML = doc;
 
